51fc0c2ccbfd14dfd62a1c39465720b78e69f3d2
dashohoxha <dashohoxha@gmail.com>
2018-04-26 18:12:24 +0200

    Improve sql-dump options

--- ds/cmd/backup.sh
+++ ds/cmd/backup.sh
@@ -41,7 +41,8 @@ _make_app_backup() {
 
     # dump the content of the databases
     ds exec drush @$app sql-dump \
-           --result-file=/host/$backup/$app.sql
+       --extra="--hex-blob --compress" \
+       --result-file=/host/$backup/$app.sql
 
     # copy app files to the backup dir
     cp -a var-www/$app $backup
@@ -69,6 +70,7 @@ _make_full_backup() {
 
     # dump the content of the database
     ds exec drush @bcl sql-dump \
+       --extra="--hex-blob --compress" \
        --result-file=/host/$backup/bcl.sql
 
     # copy app files to the backup dir
@@ -77,6 +79,7 @@ _make_full_backup() {
     # backup also the bcl_dev
     if [[ -d var-www/bcl_dev ]]; then
         ds exec drush @bcl_dev sql-dump \
+           --extra="--hex-blob --compress" \
            --result-file=/host/$backup/bcl_dev.sql
         cp -a var-www/bcl_dev $backup/
     fi
