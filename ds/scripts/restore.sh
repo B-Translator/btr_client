6d2017b6cd46ee41617186243e2dab61bdb4d5c5
Dashamir Hoxha <dashohoxha@gmail.com>
2018-11-25 04:44:34 +0100

    Improve restore and remake

--- ds/scripts/restore.sh
+++ ds/scripts/restore.sh
@@ -1,11 +1,30 @@
 #!/bin/bash -x
 
+restore_custom_scripts() {
+    if [[ ! -f /host/backup.sh ]] && [[ -f backup.sh ]]; then
+        cp backup.sh /host/
+    fi
+    if [[ ! -f /host/restore.sh ]] && [[ -f restore.sh ]]; then
+        cp restore.sh /host/
+    fi
+    if [[ ! -d /host/cmd ]] && [[ -d cmd ]]; then
+        cp -a cmd /host/
+    fi
+    if [[ ! -d /host/scripts ]] && [[ -d scripts ]]; then
+        cp -a scripts /host/
+    fi
+}
+
 # go to the backup directory
 backup=$1
 cd /host/$backup
 
 # restore bcl users
 drush @bcl sql-query --file=$(pwd)/bcl_users.sql
+
+# delete any existing content
+drush --yes @bcl pm-enable delete_all
+drush --yes @bcl delete-all all --reset
 
 # enable features
 while read feature; do
@@ -19,12 +38,8 @@ drush @bcl php-script $(pwd)/restore-private-vars-bcl.php
 # restore twitter configuration
 [[ -f trc ]] && cp trc /home/twitter/.trc
 
+# restore any custom scripts
+restore_custom_scripts
+
 # custom restore script
 [[ -f /host/restore.sh ]] && source /host/restore.sh
-
-# restore any custom scripts
-[[ -f /host/backup.sh ]] || cp backup.sh /host/
-[[ -f /host/restore.sh ]] || cp restore.sh /host/
-[[ -d /host/cmd ]] || cp -a cmd /host/
-[[ -d /host/scripts ]] || cp -a scripts /host/
-
